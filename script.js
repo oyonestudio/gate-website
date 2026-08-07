910af2bb5e5820518061152f29c25a271aace68c
oyonestudio <miyuanjin5@gmail.com>
2026-08-07 22:41:14 +0900

    add: ?????????????????

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { title: "漫画吹き出しUI", date: "2026.08.03", id: "1JvxYFgarPZ79xJFHf_zsXsiBxt8AAh0A" },
     { title: "インタラクト変更", date: "2026.08.03", id: "1anIlzipYly7HP5b7ieuZUKh5lPX-_sXM" },
     { title: "日本ぽいインタラクト改", date: "2026.08.05", id: "1cXB8RLjAj0ejr_-UCNBaD3kN09FJ-bhT" },
+    { title: "掴み演出実装", date: "2026.08.07", id: "1J1KG539x4rlTJnKWFdnpzD34tWl5muG6" },
   ];
 
   const grid = document.getElementById('devlogGrid');
